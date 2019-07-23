8d565cd8bf80dadcf05af0c20e4d2a164d7abc0f
ericbowman <ericjbowman@gmail.com>
2019-07-23 11:08:18 -0400

    Redirect and add snack for Delete success

--- src/Routine.js
+++ src/Routine.js
@@ -1,10 +1,19 @@
 import React, { Component, Fragment } from 'react'
 import Wave from './Wave'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Redirect } from 'react-router-dom'
+import { withSnackbar } from 'notistack'
 import axios from 'axios'
 import apiUrl from './apiConfig'
+import messages from './auth/messages'
 
-class Routines extends Component {
+class Routine extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      deleted: false
+    }
+  }
+
   deleteRoutine = () => {
     axios({
       url: (`${apiUrl}/inputs/${this.props.id}`),
@@ -13,35 +22,46 @@ class Routines extends Component {
         'Authorization': `Token token=${this.props.user.token}`
       }
     })
+      .then(() => this.setState({ deleted: true }))
+      .then(() => this.props.enqueueSnackbar(messages.routineDestroySuccess, { variant: 'success' }))
   }
   render () {
     const { routine, id } = this.props
-    return (
-      <Fragment id={id} key={id}>
-        <h3>{routine.title}</h3>
-        <button onClick={this.deleteRoutine}>Delete</button>
-        <button>Edit</button>
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <Wave wave={routine.wave1} number='1' wave1={routine.wave1}/>
+    const { deleted } = this.state
+    if (deleted) {
+      return (
+        <Redirect to={
+          { pathname: '/', status: { msg: 'Routine Successfully Deleted' } }
+        } />
+      )
+    } if (!deleted) {
+      return (
+        <Fragment id={id} key={id}>
+          <h3>{routine.title}</h3>
+          <button onClick={this.deleteRoutine}>Delete</button>
+          <button>Edit</button>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <Wave wave={routine.wave1} number='1' wave1={routine.wave1}/>
+              </div>
+              <div className="col-6">
+                <Wave wave={routine.wave2} number='2'wave1={routine.wave1}/>
+              </div>
             </div>
-            <div className="col-6">
-              <Wave wave={routine.wave2} number='2'wave1={routine.wave1}/>
+            <div className="row">
+              <div className="col-6">
+                <Wave wave={routine.wave3} number='3'wave1={routine.wave1}/>
+              </div>
+              <div className="col-6">
+                <Wave wave={routine.wave4} number='4'wave1={routine.wave1}/>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-6">
-              <Wave wave={routine.wave3} number='3'wave1={routine.wave1}/>
-            </div>
-            <div className="col-6">
-              <Wave wave={routine.wave4} number='4'wave1={routine.wave1}/>
-            </div>
-          </div>
-        </div>
-      </Fragment>
-    )
+        </Fragment>
+      )
+    }
   }
 }
 
-export default withRouter(Routines)
+export default withSnackbar(withRouter(Routine))
